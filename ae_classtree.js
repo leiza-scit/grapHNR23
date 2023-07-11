c85399a09418f71d583224344a4279a76816ab95
florianthiery <mail@fthiery.de>
2023-07-11 21:28:36 +0000

    Deploying to gh-pages from @ leiza-scit/grapHNR23@7d5024509a43edcb6fa8bee3f8cb8c259a29308a 🚀

---
+++ ae_classtree.js
@@ -0,0 +1,290 @@
+var tree={
+  "plugins": [
+    "defaults",
+    "search",
+    "sort",
+    "state",
+    "types",
+    "contextmenu"
+  ],
+  "search": {
+    "show_only_matches": true
+  },
+  "types": {
+    "class": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/class.png"
+    },
+    "geoclass": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/geoclass.png"
+    },
+    "halfgeoclass": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/halfgeoclass.png"
+    },
+    "collectionclass": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/collectionclass.png"
+    },
+    "geocollection": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/geometrycollection.png"
+    },
+    "featurecollection": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/featurecollection.png"
+    },
+    "instance": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/instance.png"
+    },
+    "geoinstance": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/geoinstance.png"
+    }
+  },
+  "core": {
+    "themes": {
+      "responsive": true
+    },
+    "check_callback": true,
+    "data": [
+      {
+        "id": "http://data.archaeology.link/data/ae/GAkXm7_suniv0_",
+        "parent": "http://archaeology.link/ontology#event",
+        "type": "instance",
+        "text": "AlbLimes (ae:GAkXm7)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/aGrEeP_suniv0_",
+        "parent": "http://archaeology.link/ontology#event",
+        "type": "instance",
+        "text": "DonauLimesPhase1 (ae:aGrEeP)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/lKvwGk_suniv0_",
+        "parent": "http://archaeology.link/ontology#event",
+        "type": "instance",
+        "text": "DonauLimesPhase2 (ae:lKvwGk)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/M7pzKk_suniv0_",
+        "parent": "http://archaeology.link/ontology#event",
+        "type": "instance",
+        "text": "Elisabethenstrasse (ae:M7pzKk)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/Ye7K01_suniv0_",
+        "parent": "http://archaeology.link/ontology#event",
+        "type": "instance",
+        "text": "HadriansWall (ae:Ye7K01)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/mL1qew_suniv0_",
+        "parent": "http://archaeology.link/ontology#event",
+        "type": "instance",
+        "text": "Neckarlimes (ae:mL1qew)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/zGVB42_suniv0_",
+        "parent": "http://archaeology.link/ontology#event",
+        "type": "instance",
+        "text": "NoordzeeKust (ae:zGVB42)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/Me2Pl1_suniv0_",
+        "parent": "http://archaeology.link/ontology#event",
+        "type": "instance",
+        "text": "Wetteraulimes (ae:Me2Pl1)",
+        "data": {}
+      },
+      {
+        "id": "http://archaeology.link/ontology#event",
+        "parent": "#",
+        "type": "class",
+        "text": "event (alligator:event) [8]",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/GAkXm7_suniv1_",
+        "parent": "http://www.w3.org/2006/time#Interval",
+        "type": "instance",
+        "text": "AlbLimes (ae:GAkXm7)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/aGrEeP_suniv1_",
+        "parent": "http://www.w3.org/2006/time#Interval",
+        "type": "instance",
+        "text": "DonauLimesPhase1 (ae:aGrEeP)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/lKvwGk_suniv1_",
+        "parent": "http://www.w3.org/2006/time#Interval",
+        "type": "instance",
+        "text": "DonauLimesPhase2 (ae:lKvwGk)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/M7pzKk_suniv1_",
+        "parent": "http://www.w3.org/2006/time#Interval",
+        "type": "instance",
+        "text": "Elisabethenstrasse (ae:M7pzKk)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/Ye7K01_suniv1_",
+        "parent": "http://www.w3.org/2006/time#Interval",
+        "type": "instance",
+        "text": "HadriansWall (ae:Ye7K01)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/mL1qew_suniv1_",
+        "parent": "http://www.w3.org/2006/time#Interval",
+        "type": "instance",
+        "text": "Neckarlimes (ae:mL1qew)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/zGVB42_suniv1_",
+        "parent": "http://www.w3.org/2006/time#Interval",
+        "type": "instance",
+        "text": "NoordzeeKust (ae:zGVB42)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/Me2Pl1_suniv1_",
+        "parent": "http://www.w3.org/2006/time#Interval",
+        "type": "instance",
+        "text": "Wetteraulimes (ae:Me2Pl1)",
+        "data": {}
+      },
+      {
+        "id": "http://www.w3.org/2006/time#Interval",
+        "parent": "#",
+        "type": "class",
+        "text": "Interval (time:Interval) [8]",
+        "data": {
+          "to": {
+            "http://archaeology.link/ontology#cax": {
+              "instancecount": 1
+            },
+            "http://archaeology.link/ontology#cay": {
+              "instancecount": 1
+            },
+            "http://archaeology.link/ontology#caz": {
+              "instancecount": 1
+            },
+            "http://archaeology.link/ontology#endfixed": {
+              "instancecount": 1
+            },
+            "http://archaeology.link/ontology#estimatedend": {
+              "instancecount": 1
+            },
+            "http://archaeology.link/ontology#estimatedstart": {
+              "instancecount": 1
+            },
+            "http://archaeology.link/ontology#startfixed": {
+              "instancecount": 1
+            },
+            "http://purl.org/dc/elements/1.1/identifier": {
+              "instancecount": 1
+            },
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": {
+              "instancecount": 2
+            },
+            "http://www.w3.org/2000/01/rdf-schema#label": {
+              "instancecount": 1
+            },
+            "http://www.w3.org/2006/time#intervalAfter": {
+              "instancecount": 1,
+              "http://archaeology.link/ontology#event": 1,
+              "http://www.w3.org/2006/time#Interval": 1
+            },
+            "http://www.w3.org/2006/time#intervalFinishedBy": {
+              "instancecount": 4,
+              "http://archaeology.link/ontology#event": 4,
+              "http://www.w3.org/2006/time#Interval": 4
+            },
+            "http://www.w3.org/2006/time#intervalFinishes": {
+              "instancecount": 1,
+              "http://archaeology.link/ontology#event": 1,
+              "http://www.w3.org/2006/time#Interval": 1
+            },
+            "http://www.w3.org/2006/time#intervalOverlappedBy": {
+              "instancecount": 1,
+              "http://archaeology.link/ontology#event": 1,
+              "http://www.w3.org/2006/time#Interval": 1
+            }
+          },
+          "from": {
+            "http://www.w3.org/2000/01/rdf-schema#member": {
+              "instancecount": 0,
+              "http://www.w3.org/2004/02/skos/core#Collection": 2
+            },
+            "http://www.w3.org/2006/time#intervalBefore": {
+              "instancecount": 0,
+              "http://archaeology.link/ontology#event": 1,
+              "http://www.w3.org/2006/time#Interval": 1
+            },
+            "http://www.w3.org/2006/time#intervalFinishedBy": {
+              "instancecount": 0,
+              "http://archaeology.link/ontology#event": 1,
+              "http://www.w3.org/2006/time#Interval": 1
+            },
+            "http://www.w3.org/2006/time#intervalFinishes": {
+              "instancecount": 0,
+              "http://archaeology.link/ontology#event": 4,
+              "http://www.w3.org/2006/time#Interval": 4
+            },
+            "http://www.w3.org/2006/time#intervalOverlaps": {
+              "instancecount": 0,
+              "http://archaeology.link/ontology#event": 1,
+              "http://www.w3.org/2006/time#Interval": 1
+            }
+          }
+        },
+        "instancecount": 18
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/event_collection",
+        "parent": "http://www.w3.org/2004/02/skos/core#Collection",
+        "type": "instance",
+        "text": "event Instances Collection (ae:event_collection)",
+        "data": {}
+      },
+      {
+        "id": "http://data.archaeology.link/data/ae/Interval_collection",
+        "parent": "http://www.w3.org/2004/02/skos/core#Collection",
+        "type": "instance",
+        "text": "Interval Instances Collection (ae:Interval_collection)",
+        "data": {}
+      },
+      {
+        "id": "http://www.w3.org/2004/02/skos/core#Collection",
+        "parent": "#",
+        "type": "collectionclass",
+        "text": "Collection (skos:Collection) [2]",
+        "data": {
+          "to": {
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": {
+              "instancecount": 1
+            },
+            "http://www.w3.org/2000/01/rdf-schema#label": {
+              "instancecount": 1
+            },
+            "http://www.w3.org/2000/01/rdf-schema#member": {
+              "instancecount": 8,
+              "http://archaeology.link/ontology#event": 8,
+              "http://www.w3.org/2006/time#Interval": 8
+            }
+          },
+          "from": {}
+        },
+        "instancecount": 10
+      }
+    ]
+  }
+}
---
+++ ae_search.js
@@ -0,0 +1,12 @@
+var search={
+  "AlbLimes": "http://data.archaeology.link/data/ae/GAkXm7",
+  "DonauLimesPhase1": "http://data.archaeology.link/data/ae/aGrEeP",
+  "DonauLimesPhase2": "http://data.archaeology.link/data/ae/lKvwGk",
+  "Elisabethenstrasse": "http://data.archaeology.link/data/ae/M7pzKk",
+  "HadriansWall": "http://data.archaeology.link/data/ae/Ye7K01",
+  "Interval Instances Collection": "http://data.archaeology.link/data/ae/Interval_collection",
+  "Neckarlimes": "http://data.archaeology.link/data/ae/mL1qew",
+  "NoordzeeKust": "http://data.archaeology.link/data/ae/zGVB42",
+  "Wetteraulimes": "http://data.archaeology.link/data/ae/Me2Pl1",
+  "event Instances Collection": "http://data.archaeology.link/data/ae/event_collection"
+}
